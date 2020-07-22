c9b55d2395253d6458f69a55545c620a7773034e
LucasMagnana <lucas.magnana@gmail.com>
2020-07-22 20:56:39 +0200

    graph modification

--- test.py
+++ test.py
@@ -23,6 +23,8 @@ from sklearn.neighbors import KDTree
 import folium
 import matplotlib.pyplot as plt
 
+import datetime
+
 import python.data as data
 import python.display as dp
 import python.voxels as voxel
@@ -167,158 +169,25 @@ for i in range(len(dict_cluster[cl])):
     df_cluster = df_cluster.append(df_temp)
 _, dict_voxels_cluster = voxel.create_dict_vox(df_cluster, 1, df_cluster.iloc[-1]["route_num"])
 
-dist_start = 999999999
-dist_end = 99999999
-start_point = [df_route.iloc[0]["lat"], df_route.iloc[0]["lon"]]
-end_point = [df_route.iloc[-1]["lat"], df_route.iloc[-1]["lon"]]
-for vox in dict_voxels_cluster:
-    if(dict_voxels_cluster[vox]["cyclability_coeff"]>coeff_min):
-        vox_str = vox.split(";")
-        vox_int = [int(vox_str[0]), int(vox_str[1])]
-        point_vox = voxel.get_voxel_points(vox_int)
-        point_vox = [point_vox[0][0], point_vox[0][1]]
-        dist = data.distance_between_points(start_point, point_vox)
-        if(dist < dist_start):
-            dist_start = data.distance_between_points(end_point, point_vox)
-            vox_start = vox
-            vox_point_start = point_vox
-        dist = data.distance_between_points(end_point, point_vox)
-        if(dist < dist_end):
-            dist_end = data.distance_between_points(start_point, point_vox)
-            vox_end = vox
-            vox_point_end = point_vox
+print("start:", datetime.datetime.now().time())
+for v in G:
+    for v_n in G[v]:
+        df_line = pd.DataFrame([[G.nodes[v]['y'], G.nodes[v]['x'], 1], [G.nodes[v_n]['y'], G.nodes[v_n]['x'], 1]], columns=["lat", "lon", "route_num"])
+        tab_voxels, _ = voxel.create_dict_vox(df_line, 1, 1)
+        nb_vox_found = 0
+        tot_coeff = 0
+        for vox in tab_voxels[0]:
+            if vox in dict_voxels_cluster:
+                nb_vox_found += 1
+                tot_coeff += dict_voxels_cluster[vox]["cyclability_coeff"]
+            if(nb_vox_found > 0):
+                tot_coeff /= nb_vox_found
+        G[v][v_n][0]['length'] -= G[v][v_n][0]['length']*(tot_coeff/1.7)
 
+print("end:", datetime.datetime.now().time())
 
-
-
-
-vox = vox_start
-
-distance = data.distance_between_points(end_point, vox_point_end)
-tab_vox_start = []
-while(vox != None):
-    tab_vox_start.append(vox)
-    vox, distance = test_deviation(vox, end_point, distance, dict_voxels_cluster)
-
-vox_str = tab_vox_start[-1].split(";")
-vox_int = [int(vox_str[0]), int(vox_str[1])]
-point_vox = voxel.get_voxel_points(vox_int)
-
-route = data.pathfind_route_osmnx(end_point, [point_vox[0][0], point_vox[0][1]], tree, G)
-route_coord = [[G.nodes[x]["x"], G.nodes[x]["y"]] for x in route]
-route_coord = [x + [1, 1] for x in route_coord]
-
-df_route_modified = pd.DataFrame(route_coord, columns=["lon", "lat", "route_num", "type"])  
-
-
-
-
-vox = vox_end
-distance = dist_end
-tab_vox_end = []
-while(vox != None):
-    tab_vox_end.append(vox)
-    vox, distance = test_deviation(vox, start_point, distance, dict_voxels_cluster)
-
-vox_str = tab_vox_end[-1].split(";")
-vox_int = [int(vox_str[0]), int(vox_str[1])]
-point_vox = voxel.get_voxel_points(vox_int)
-
-route = data.pathfind_route_osmnx(start_point, [point_vox[0][0], point_vox[0][1]], tree, G)
+route = data.pathfind_route_osmnx(d_point, f_point, tree, G)
 route_coord = [[G.nodes[x]["x"], G.nodes[x]["y"]] for x in route]
 route_coord = [x + [2, 1] for x in route_coord]
-df_route_modified = df_route_modified.append(pd.DataFrame(route_coord, columns=["lon", "lat", "route_num", "type"])  )
-
-
-if(tab_vox_end[0] != tab_vox_start[0]):
-    vox_str = tab_vox_end[0].split(";")
-    vox_int = [int(vox_str[0]), int(vox_str[1])]
-    point_vox_end = voxel.get_voxel_points(vox_int)
-
-    vox_str = tab_vox_start[0].split(";")
-    vox_int = [int(vox_str[0]), int(vox_str[1])]
-    point_vox_start = voxel.get_voxel_points(vox_int)
-
-    route = data.pathfind_route_osmnx([point_vox_start[0][0], point_vox_start[0][1]], [point_vox_end[0][0], point_vox_end[0][1]], tree, G)
-    route_coord = [[G.nodes[x]["x"], G.nodes[x]["y"]] for x in route]
-    route_coord = [x + [3, 1] for x in route_coord]
-    df_route_modified = df_route_modified.append(pd.DataFrame(route_coord, columns=["lon", "lat", "route_num", "type"])  )
-
-
-
-
-#AFFICHAGE
-
-tab_points_vox_start = []
-for i in range(len(tab_vox_start)):
-    key_vox = tab_vox_start[i]
-    vox_str = key_vox.split(";")
-    vox_int = [int(vox_str[0]), int(vox_str[1])]
-    vox_points = voxel.get_voxel_points(vox_int, -1-i)
-    tab_points_vox_start.extend(vox_points)
-
-
-df = pd.DataFrame(tab_points_vox_start, columns=["lat", "lon", "route_num", "type"])
-
-tab_points_vox_end = []
-for i in range(len(tab_vox_end)):
-    key_vox = tab_vox_end[i]
-    vox_str = key_vox.split(";")
-    vox_int = [int(vox_str[0]), int(vox_str[1])]
-    vox_points = voxel.get_voxel_points(vox_int, -len(tab_points_vox_start)-1-i)
-    tab_points_vox_end.extend(vox_points)
-
-df_end = pd.DataFrame(tab_points_vox_end, columns=["lat", "lon", "route_num", "type"])
-df = df.append(df_end)
-df = df.append(df_route_modified)
-
-dp.display(df)
-
-
-
-
-
-"""nodes, _ = ox.graph_to_gdfs(G)
-last_node = None
-
-for p in range(len(df_route)-1):
-    if(p == 0):
-        point = [df_route.iloc[p]["lat"], df_route.iloc[p]["lon"]]
-        df_route_modified = pd.DataFrame([[point[0], point[1], 1]], columns=["lat", "lon", "route_num"])
-    else:
-        df_route_modified = df_route_modified.append(pd.DataFrame([[point[0], point[1], 1]], columns=["lat", "lon", "route_num"]))
-    d_idx = tree.query([point], k=1, return_distance=False)[0]
-    node = nodes.iloc[d_idx].index.values[0]
-    max_coeffs = 0
-    next_node = -1
-    for n in G.neighbors(node):
-        if(n != last_node and ((df_route_modified['lat'] == point[0]) & (df_route_modified['lon'] == point[1])).any()):
-            df_seg = pd.DataFrame([[point[0],point[1], 1]], columns=["lat", "lon", "route_num"])
-            df_seg = df_seg.append(pd.DataFrame([[G.nodes[n]['y'], G.nodes[n]['x'], 1]], columns=["lat", "lon", "route_num"]))
-
-            tab_routes_voxels, _ = voxel.create_dict_vox(df_seg, 1, 1)
-            coeffs = 0
-            for vox in tab_routes_voxels[0] :
-                if vox in dict_voxels_cluster :
-                    coeffs += dict_voxels_cluster[vox]["cyclability_coeff"]
-            coeffs /= len(tab_routes_voxels[0])
-            if(coeffs > max_coeffs):
-                max_coeffs = coeffs
-                next_node = n
-
-    next_point = [df_route.iloc[p+1]["lat"], df_route.iloc[p+1]["lon"]]
-    last_node = node
-
-    if(next_node != -1):
-        next_point_cluster = [G.nodes[next_node]['y'], G.nodes[next_node]['x']]
-        if(next_point_cluster[0] != next_point[0] or next_point_cluster[1] != next_point[1]):
-            point = next_point_cluster
-        else: 
-            point = next_point
-    else:
-        point = next_point
-
-print(df_route_modified)
+df_route_modified = pd.DataFrame(route_coord, columns=["lon", "lat", "route_num", "type"])
 dp.display(df_route_modified)
-
-"""
